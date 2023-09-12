8cb74250b4b9685bf5cb1b11b52da6c2a87a6a4d
Sergey Lvov <SergeyLS@dmigom.ru>
2023-09-12 16:34:34 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Configuration;
+using Confluent.Kafka;
+// using Microsoft.Extensions.Configuration;
+
+class Program
+{
+	private static void RunConsumer()
+	{
+		ConsumerConfig config = new ConsumerConfig
+		{
+			BootstrapServers = ConfigurationManager.AppSettings["kafka"],
+			GroupId = "test",
+			AutoOffsetReset = AutoOffsetReset.Earliest
+		};
+		IConsumer<string, string> consumer = new ConsumerBuilder< string, string>(config).Build();
+		consumer.Subscribe("test");
+
+		var result = consumer.Consume();
+
+		Console.WriteLine("Consumed: " + result.Value);
+	}
+	public static async  Task Main()
+	{
+		Console.WriteLine("kafka");
+		try
+		{
+			Thread consume = new Thread(() =>
+			{
+				RunConsumer();
+			});
+			
+			ProducerConfig config = new ProducerConfig()
+			{
+				BootstrapServers = ConfigurationManager.AppSettings["kafka"]
+			};
+			IProducer<string, string> producer = new ProducerBuilder<string, string>(config).Build();
+			var result = await producer.ProduceAsync("test", new Message<string, string>() { Key = "first", Value = "first message" });
+			Console.WriteLine("Result: " + result.Status.ToString());
+			consume.Start();
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine("Exception: " + e.Message);
+		}
+
+	}
+}
+	
